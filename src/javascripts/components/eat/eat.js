a2390342160b103c6f310f830b8b5cd61c701cf4
Liza Star <lizadstar18@gmail.com>
2020-03-07 13:20:21 -0600

    eat div works

---
+++ src/javascripts/components/eat/eat.js
@@ -0,0 +1,13 @@
+import eatData from '../../helper/data/eatData';
+import utils from '../../helper/utils';
+
+const eatBuilder = () => {
+  const fullScore = eatData.getFullScore();
+  let domString = '';
+  domString += '<h3> Eat </h3>';
+  domString += `<h5> Full Score: ${fullScore} </h5>`;
+  domString += '<button id="healthy-food"> Healthy Food </button><button id="unhealthy-food"> Unhealthy Food </button>';
+  utils.printToDom('eat', domString);
+};
+
+export default { eatBuilder };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,21 @@
 import '../styles/main.scss';
+import eat from './components/eat/eat';
+import eatData from './helper/data/eatData';
 
-console.error('hey');
+const addToFullScore = () => {
+  eatData.setFullScore(10);
+  eat.eatBuilder();
+};
+
+const subFromFullScore = () => {
+  eatData.setFullScore(-3);
+  eat.eatBuilder();
+};
+
+const init = () => {
+  eat.eatBuilder();
+  $('#eat').on('click', '#healthy-food', addToFullScore);
+  $('#eat').on('click', '#unhealthy-food', subFromFullScore);
+};
+
+init();
